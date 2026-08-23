f8b5bc38c4f42b8ce82f74d0f6fbae38304242cf
robertdcconsultor-web <robertdc.consultor@gmail.com>
2026-08-23 13:17:56 -0300

    Depois eu formato o PC, finalizado MotorVistoria em Services!

--- src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
+++ src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
@@ -6,19 +6,42 @@ namespace AutoCheck.ConsoleApp.Services
 {
     public class MotorVistoria
     {
+        
         public int PontuacaoObtida {get; private set;}
         public int PontuacaoMaxima {get; private set;}
 
-        public double PercentualAprovado {get; private set;}
+        public double PercentualAprovacao {get; private set;}
 
         public double ClassificacaoFinal {get; private set;}
 
         public void Processar(Veiculo veiculo)
         {
-            this.PontuacaoMaxima = 0;
-            this.PontuacaoObtida = veiculo.VistoriaRealizada.Count * 10;
+            this.PontuacaoObtida = 0;
+            this.PontuacaoMaxima = veiculo.VistoriaRealizada.Count * 10;
 
-            foreach (ItemVistoria item in vei)
+            //Atende aos critérios de pontuação, poderia ter apenas colocado 1 para ruim, 2 para regular e 3 pra bom, mas dá no mesmo!
+            foreach (ItemVistoria item in veiculo.VistoriaRealizada)
+            {
+                if (item.Status == "Bom") this.PontuacaoObtida += 10;
+                else if (item.Status == "Regular") this.PontuacaoObtida += 5;
+            }
+            if (this.PontuacaoMaxima > 0)
+            {
+                //Atende a somatória solicita
+                this.PercentualAprovacao = this.PontuacaoObtida / this.PontuacaoMaxima) *100;             
+            }
+            else
+            {
+                this.PercentualAprovacao = 0;
+            }
+
+            ClassificarVeiculo();
+        }
+        private void ClassificarVeiculo()
+        {
+            if (this.PercentualAprovacao >=90) this.ClassificacaoFinal = "Aprovado com Excelência";
+            else if (this.PercentualAprovacao >=60) this.ClassificacaoFinal = "Aprovado com Apontamentos";
+            else this.ClassificacaoFinal = "Reprovado na Vistoria";
         }
     }
 }
