e2775d3ebf8c0a689168de0df9802af17b44db2f
robertdcconsultor-web <robertdc.consultor@gmail.com>
2026-08-23 12:17:19 -0300

    Iniciando MotorVistoria - Serviço - mas parece que fui contaminado vou formatar o PC

---
+++ src/AutoCheck.ConsoleApp/Services/MotorVistoria.cs
@@ -0,0 +1,24 @@
+﻿
+
+using AutoCheck.ConsoleApp.Models;
+
+namespace AutoCheck.ConsoleApp.Services
+{
+    public class MotorVistoria
+    {
+        public int PontuacaoObtida {get; private set;}
+        public int PontuacaoMaxima {get; private set;}
+
+        public double PercentualAprovado {get; private set;}
+
+        public double ClassificacaoFinal {get; private set;}
+
+        public void Processar(Veiculo veiculo)
+        {
+            this.PontuacaoMaxima = 0;
+            this.PontuacaoObtida = veiculo.VistoriaRealizada.Count * 10;
+
+            foreach (ItemVistoria item in vei)
+        }
+    }
+}
